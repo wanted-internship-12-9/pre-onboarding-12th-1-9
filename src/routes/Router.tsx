ba2e94be81f4da54e86c06da0011e740a56d26ca
cham2 <dearcham2@gmail.com>
2023-08-23 20:26:37 +0900

    ✨ feat #3: 토큰 유무에 따라 리다이렉트를 처리하는 AuthLayout 컴포넌트 구현

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -5,15 +5,18 @@ import RootPage from '../pages/RootPage';
 import SignUpPage from '../pages/SignUpPage';
 import SignInPage from '../pages/SignInPage';
 import TodoPage from '../pages/TodoPage';
+import AuthLayout from '../components/AuthLayout';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RootPage />} />
-        <Route path="signup" element={<SignUpPage />} />
-        <Route path="signin" element={<SignInPage />} />
-        <Route path="todo" element={<TodoPage />} />
+        <Route element={<AuthLayout />}>
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="signin" element={<SignInPage />} />
+          <Route path="todo" element={<TodoPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
