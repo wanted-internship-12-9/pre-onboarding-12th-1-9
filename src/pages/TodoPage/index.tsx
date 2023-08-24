0dbd647f096f5c84bd97b1763b093b2f2a2b9138
hyunwlee-dev <hwlee3928@gmail.com>
2023-08-24 19:34:20 +0900

    :zap: Feat #7: getTodos api 추가

--- src/pages/TodoPage/index.tsx
+++ src/pages/TodoPage/index.tsx
@@ -1,13 +1,29 @@
-import * as React from 'react';
+import { useEffect, useState } from 'react';
+import { API_PATH } from '../../api/apiConfig';
+import { axiosFetch } from '../../api/axiosInstance';
 import {
-  TodoArea,
   TodoContainer,
+  TodoArea,
   TodoInputArea,
   TodoListArea,
   TodoTitle,
 } from '../../styles/todoStyles.style';
+import { TodoResponse } from '../../types/todo';
 
 function TodoList() {
+  const [todoList, setTodoList] = useState<TodoResponse[]>();
+  console.info('todoList: ', todoList);
+  useEffect(() => {
+    getTodos();
+  }, [])
+  const getTodos = async () => {
+    try {
+      const result = await axiosFetch.get(API_PATH.TODOS);
+      setTodoList(result?.data);
+    } catch (err) {
+      alert(err);
+    }
+  };
   return (
     <TodoContainer>
       <TodoArea>
