d483be660e05c7782659dc0b5c2aa43121aeada2
cham2 <dearcham2@gmail.com>
2023-08-25 12:59:25 +0900

    ♻️  refactor #29: useEffect, useNavigate로 리다이렉트 처리 -> Navigate를 사용하여 리다이렉트 처리 방식으로 변경

--- src/components/AuthLayout/index.tsx
+++ src/components/AuthLayout/index.tsx
@@ -1,23 +1,21 @@
-import React, { useEffect } from 'react';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
 const AuthLayout = () => {
   const location = useLocation();
-  const navigate = useNavigate();
+
   const currentPath = location.pathname;
   const accessToken = localStorage.getItem('accessToken');
 
-  useEffect(() => {
-    if (currentPath === '/todo' && !accessToken) {
-      navigate('/signin');
-    }
-    if (
-      (currentPath === '/' || currentPath === '/signin' || currentPath === '/signup') &&
-      accessToken
-    ) {
-      navigate('/todo');
-    }
-  }, [currentPath, accessToken]);
+  if (currentPath === '/todo' && !accessToken) {
+    return <Navigate to="/signin" />;
+  }
+  if (
+    (currentPath === '/' || currentPath === '/signin' || currentPath === '/signup') &&
+    accessToken
+  ) {
+    return <Navigate to="/todo" />;
+  }
 
   return <Outlet />;
 };
